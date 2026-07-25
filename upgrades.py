05ca8cddfcc52250604250d8de2a9db7789a1616
fedi41-prog <crocofedi4@gmail.com>
2026-07-25 16:42:58 +0200

    kinda finished the upgrade class....

--- upgrades.py
+++ upgrades.py
@@ -1,3 +1,5 @@
+from pygame.display import update
+
 from player import Player
 
 
@@ -7,13 +9,41 @@ from player import Player
 #
 #
 
-class UpgradeManager:
+
+
+
+class Upgrades:
+    SPEED = 0
+    FIRE_RATE = 1
+
+
+
     def __init__(self):
-        self.upgrade_table = [
-            0
+        self.upgrades = [
+            SpeedUpgrade(),
+            FireRateUpgrade()
         ]
+
+    def apply(self, player:Player):
+        for upgrade in self.upgrades:
+            upgrade.apply(player)
 
 
 
 class Upgrade:
-    def update(self, ,player:Player):
+    def __init__(self, level=0):
+        self.level = 0
+    def apply(self, player:Player):
+        pass
+    def next_level(self):
+        self.level += 1
+    def update_level(self, new_level:int):
+        self.level = new_level
+
+class SpeedUpgrade(Upgrade):
+    def apply(self, player:Player):
+        player.speed = 100 + self.level * 50
+class FireRateUpgrade(Upgrade):
+    def apply(self, player:Player):
+        player.shoot_cooldown = 1 / (self.level + 1)
+
