e319665450ee9c142bb58970cc0d6f511465f8bf
fedi41-prog <crocofedi4@gmail.com>
2026-07-25 13:53:26 +0200

    fix merge errors

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ while running:
     if current_enemy_spawn_counter <= 0:
         e = Enemy((
             random.randint(-20, WIDTH + 20), random.randint(-20, HEIGHT +20)
-        ))
+        ), camera)
         enemies.add(e)
         current_enemy_spawn_counter = wave
     else: 
