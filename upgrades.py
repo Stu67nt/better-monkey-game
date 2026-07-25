ff4ef694182d2a909a580d3ddbc17146d56c0682
fedi41-prog <crocofedi4@gmail.com>
2026-07-25 16:14:59 +0200

    started working on upgrades

---
+++ upgrades.py
@@ -0,0 +1,19 @@
+from player import Player
+
+
+# UPGRADE system
+# each upgrade has a level,
+#
+#
+#
+
+class UpgradeManager:
+    def __init__(self):
+        self.upgrade_table = [
+            0
+        ]
+
+
+
+class Upgrade:
+    def update(self, ,player:Player):
